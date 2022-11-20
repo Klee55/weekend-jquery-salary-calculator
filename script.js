dd947feded904ae60c7ed7ea234c16dc687e9e13
Khue Lee <khuelee55@gmail.com>
2022-11-20 14:25:15 -0600

    got salaryCalc function to work

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('onReady');
-    $('#submitButton').on('click', salaryCalc);
+    $('#submitButton').on('click', collectingEmployeeInfo);
 
 
 }
 
 let employeeInfo = [];
 
-function salaryCalc(){
+let totalSalary = 0;
+
+function collectingEmployeeInfo(){
     console.log('salaryCalc');
-    console.log(employeeInfo);
+    //console.log(employeeInfo);
     let employee = {
         firstName: $('#firstName').val(),
         lastName: $('#lastName').val(),
@@ -21,7 +23,42 @@ function salaryCalc(){
         annualSalary: $('#annualSalary').val()
     }
     employeeInfo.push(employee);
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#id').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
     console.log(employeeInfo)
+    appendEmployeeInfo();
+    salaryCalc();
+    employeeInfo.pop();
+    
 
+}
 
+function appendEmployeeInfo(){
+    console.log('appendEmployeeInfo');
+    for ( let index of employeeInfo ){
+    $('#table').append(`
+        <tbody>
+            <td>${index.firstName}</td>
+            <td>${index.lastName}</td>
+            <td>${index.id}</td>
+            <td>${index.jobTitle}</td>
+            <td>$${index.annualSalary}</td>
+        </tbody>
+    `)
+
+}
+}
+
+function salaryCalc(){
+    console.log('salaryCalc');
+        //totalNumber = 0;
+    for ( let number of employeeInfo){
+        totalSalary += Number(number.annualSalary);
+    }
+    //console.log(totalNumber);
+    console.log(totalSalary);
+    $()
 }
